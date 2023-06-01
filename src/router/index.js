fc73bb5a4285c52734d24336a17fd0c6c5c800e4
CrazyXi <xiwood@qq.com>
2023-06-01 21:19:47 +0800

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 
 const routes = [
 	{
-		path: '/',
+		path: '',
 		name: 'Home',
 		component: Home,
 		meta: {
@@ -38,7 +38,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 	scrollBehavior() {
 		document.getElementById('app').scrollIntoView();
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import path from "path"
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 export default defineConfig({
+  assetsDir:'./assets',
+  base:'./',
   plugins: [vue(), ViteMinifyPlugin({})],
   resolve: {
     alias: {
